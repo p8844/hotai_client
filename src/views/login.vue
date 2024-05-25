394e6aaa6b96a40073de0a9027dbcee6ff2c7ec9
p8844 <957442653@qq.com>
2024-05-25 14:08:43 +0800

    登录界面

--- src/views/login.vue
+++ src/views/login.vue
@@ -8,24 +8,39 @@
             </el-header>
             <el-main class="main">
                 <div class="formContainer">
-                    <el-tabs type="border-card" :stretch="true">
-                        <el-tab-pane label="登录">
-                            <el-form class="loginForm" label-width="auto" :model="loginForm" style="max-width: 600px">
-                                <el-form-item label="账 号">
+                    <el-tabs type="border-card" v-model="activeName" :stretch="true">
+                        <el-tab-pane label="登录" name="first">
+                            <el-form class="loginForm" label-width="auto" :model="loginForm" :rules="rules" style="max-width: 600px">
+                                <el-form-item label="账 号" prop="account">
                                     <el-input v-model="loginForm.account" placeholder="请输入账号" />
                                 </el-form-item>
-                                <el-form-item label="密 码">
+                                <el-form-item label="密 码" prop="password">
                                     <el-input v-model="loginForm.password" placeholder="请输入密码" />
                                 </el-form-item>
                                 <el-form-item>
-                                    <el-button type="primary" @click="" style="margin-left: 90px;">
+                                    <el-button type="primary" @click="" style="margin-left: 80px;">
                                         登录
                                     </el-button>
-                                    <el-button @click="">注册</el-button>
+                                    <el-button @click="activeName = 'second'">注册</el-button>
                                 </el-form-item>
                             </el-form>
                         </el-tab-pane>
-                        <el-tab-pane label="注册">注册</el-tab-pane>
+                        <el-tab-pane label="注册" name="second">
+                            <el-form class="registerForm" label-width="auto" :model="registerForm" :rules="rules" style="max-width: 600px">
+                                <el-form-item label="账 号" prop="account">
+                                    <el-input v-model="registerForm.account" placeholder="请输入账号" />
+                                </el-form-item>
+                                <el-form-item label="密 码" prop="password">
+                                    <el-input v-model="registerForm.password" placeholder="请输入密码" />
+                                </el-form-item>
+                                <el-form-item>
+                                    <el-button type="primary" style="margin-left: 80px;">注册</el-button>
+                                    <el-button @click="activeName = 'first'">
+                                        登录
+                                    </el-button>
+                                </el-form-item>
+                            </el-form>
+                        </el-tab-pane>
                     </el-tabs>
                 </div>
             </el-main>
@@ -34,9 +49,49 @@
 </template>
 <script setup lang="ts">
 import { ref } from 'vue'
-const loginForm = ref({
+
+const activeName = ref('first')
+
+interface UserInfo {
+    account: string
+    password: string
+}
+const loginForm = ref<UserInfo>({
     account: '',
     password: ''
+})
+const registerForm = ref<UserInfo>({
+    account: '',
+    password: ''
+})
+
+const rules = ref({
+    account: [
+        {
+            required: true,
+            message: '请输入账号',
+            trigger: 'blur'
+        },
+        {
+            min: 5,
+            max: 10,
+            message: '账号长度在 5 到 10 个字符',
+            trigger: 'blur'
+        }
+    ],
+    password: [
+        {
+            required: true,
+            message: '请输入密码',
+            trigger: 'blur'
+        },
+        {
+            min: 6,
+            max: 12,
+            message: '密码长度在 6 到 12 个字符',
+            trigger: 'blur'
+        }
+    ]
 })
 </script>
 <style scoped>
@@ -81,7 +136,9 @@ const loginForm = ref({
     left: 50%;
     transform: translate(-50%, -50%);
 }
-.main .formContainer .loginForm {
+
+.main .formContainer .loginForm,
+.main .formContainer .registerForm {
     margin-top: 20px;
 }
 </style>
